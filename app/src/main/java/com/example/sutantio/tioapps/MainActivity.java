35c6210751534d064b4c356863409247a7e21cae
Sutantio <sutantioramdhani98@gmail.com>
2019-05-05 21:51:49 +0700

    Commit2

--- app/src/main/java/com/example/sutantio/tioapps/MainActivity.java
+++ app/src/main/java/com/example/sutantio/tioapps/MainActivity.java
@@ -22,10 +22,8 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
-
-                Intent home=new Intent(MainActivity.this, Walkthroug.class);
-                startActivity(home);
+                Intent walkthrough=new Intent(MainActivity.this, Walkthroug.class);
+                startActivity(walkthrough);
                 finish();
 
             }
